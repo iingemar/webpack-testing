77de2571833c2193500beccf463a3ef3842072cc
Ingemar <iingemar@gmail.com>
2018-10-12 15:22:09 +0200

    Initial commit

---
+++ 1_sum/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+// Path needs to be absolute file system path
+// __dirname is node.js constant for the current working directory
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    }
+};
+
+module.exports = config;
---
+++ README.md
@@ -0,0 +1,2 @@
+# webpack-testing
+!!111 :D
