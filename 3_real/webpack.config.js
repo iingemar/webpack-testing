35559e261390123c284389764fbc1f11d01f4e6d
Ingemar Finn <ingemar.finn@king.com>
2018-10-31 15:15:30 +0100

    Add babel-loader

--- 3_real/webpack.config.js
+++ 3_real/webpack.config.js
@@ -2,9 +2,18 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+          {
+              use: 'babel-loader',
+              test: /\.js$/,
+              exclude: /node_modules/
+          }
+      ]
+    }
 };
