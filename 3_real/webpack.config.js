fcd5fd0433d4396cb6d872f1d6df4839bda0030c
Ingemar Finn <ingemar.finn@king.com>
2018-10-31 15:16:56 +0100

    Add css-loaders

--- 3_real/webpack.config.js
+++ 3_real/webpack.config.js
@@ -8,12 +8,16 @@ module.exports = {
         filename: 'bundle.js'
     },
     module: {
-      rules: [
-          {
-              use: 'babel-loader',
-              test: /\.js$/,
-              exclude: /node_modules/
-          }
-      ]
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                use: ['style-loader', 'css-loader'],
+                test: /\.css$/,
+            }
+        ]
     }
 };
