de8da981e2a85d57515346596e19f30db0284dd1
Ingemar Finn <ingemar.finn@king.com>
2018-11-01 09:56:48 +0100

    Add vendor bundle

--- 3_real/webpack.config.js
+++ 3_real/webpack.config.js
@@ -1,11 +1,31 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// The names of the libraries we want. The same as package.json dependencies
+const VENDOR_LIBS = [
+    'faker',
+    'lodash',
+    'react',
+    'react-dom',
+    'react-input-range',
+    'react-redux',
+    'react-router',
+    'redux',
+    'redux-form',
+    'redux-thunk'
+];
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        bundle: './src/index.js',
+        // Create another bundle file called 'vendor.js'
+        vendor: VENDOR_LIBS
+    },
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        // Save with the key from the entry section. Ie bundle.js, vendor.js
+        filename: '[name].js'
     },
     module: {
         rules: [
@@ -19,5 +39,13 @@ module.exports = {
                 test: /\.css$/,
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
 };
